d0eb232da11debedda29c3325bb53ae46625c376
Arsdrades <a.salasa@duocuc.cl>
2022-10-04 11:43:47 -0300

    Se agrega perfil

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'sign-up',
+    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
+  },
+  {
+    path: 'perfil',
+    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
+  },
+  {
+    path: 'perfil-cuenta',
+    loadChildren: () => import('./perfil-cuenta/perfil-cuenta.module').then( m => m.PerfilCuentaPageModule)
+  },
+
+  {
+    path: 'perfil-terminos',
+    loadChildren: () => import('./perfil-terminos/perfil-terminos.module').then( m => m.PerfilTerminosPageModule)
+  },
+  {
+    path: 'perfil-pedidos',
+    loadChildren: () => import('./perfil-pedidos/perfil-pedidos.module').then( m => m.PerfilPedidosPageModule)
+  },
+  {
+    path: 'perfil-escribenos',
+    loadChildren: () => import('./perfil-escribenos/perfil-escribenos.module').then( m => m.PerfilEscribenosPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/perfil-pedidos/perfil-pedidos.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-pedidos',
+  templateUrl: './perfil-pedidos.page.html',
+  styleUrls: ['./perfil-pedidos.page.scss'],
+})
+export class PerfilPedidosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
