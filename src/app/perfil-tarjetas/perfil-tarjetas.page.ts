aadde082cfe91b1c585c31e509ca9ba03c097f6a
Arsdrades <a.salasa@duocuc.cl>
2022-10-05 10:05:44 -0300

    se agregan metodos de pago

--- src/app/perfil-tarjetas/perfil-tarjetas.page.ts
+++ src/app/perfil-tarjetas/perfil-tarjetas.page.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-perfil-tarjetas',
   templateUrl: './perfil-tarjetas.page.html',
   styleUrls: ['./perfil-tarjetas.page.scss'],
 })
-export class PerfilTarjetasPage implements OnInit {
+export class PerfilTarjetasPage{
+  @ViewChild(IonModal) modal: IonModal;
 
-  constructor() { }
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name: string;
 
-  ngOnInit() {
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
   }
 
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
 }
+
